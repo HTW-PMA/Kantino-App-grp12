110c5f47aaa9ff93a2d5956a947dd80ec30849b4
Bela von Wangenheim <belavw@gmx.de>
2025-07-04 21:32:36 +0200

    overview badges

--- utils/BadgeUtils.ts
+++ utils/BadgeUtils.ts
@@ -26,3 +26,72 @@ export function isSystemBadge(name: string): boolean {
     const lower = name.toLowerCase();
     return lower.includes('co2_bewertung') || lower.includes('h2o_bewertung');
 }
+
+// Alle Badge-Namen
+/**
+Grüner Ampelpunkt
+Gelber Ampelpunkt
+Roter Ampelpunkt
+Vegan
+Fairtrade
+Klimaessen
+Vegetarisch
+Nachhaltige Landwirtschaft
+Nachhaltige Fischerei
+CO2_bewertung_A
+CO2_bewertung_B
+CO2_bewertung_C
+H2O_bewertung_A
+H2O_bewertung_B
+H2O_bewertung_C
+*/
+
+// Allergene und Zusatzstoffe
+/**
+ Schweinefleisch bzw. mit Gelatine vom Schwein
+ Alkohol
+ Geschmacksverstärker
+ gewachst
+ konserviert
+ Antioxidationsmittel
+ Farbstoff
+ Phosphat
+ geschwärzt
+ enthält eine Phenylalaninquelle
+ Süßungsmittel
+ mit zum Teil fein zerkleinertem Fleischanteil
+ koffeinhaltig
+ chininhaltig
+ geschwefelt
+ kann abführend wirken
+ Glutenhaltiges Getreide
+ Weizen
+ Roggen
+ Gerste
+ Hafer
+ Dinkel
+ Kamut
+ Krebstiere
+ Eier
+ Fisch
+ Erdnüsse
+ Schalenfrüchte
+ Mandeln
+ Haselnuss
+ Walnuss
+ Kaschunuss
+ Pacannuss
+ Paranuss
+ Pistazie
+ Macadamia
+ Sellerie
+ Soja
+ Senf
+ Milch und Milchprodukte (inkl. Laktose)
+ Sesam
+ Schwefeldioxid und Sulfide
+ Lupine
+ Weichtiere
+ Nitritpökelsalz
+ Hefe
+ */
